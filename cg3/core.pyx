4395401fa5bd8020c1835aada2df37edfee603ed
Bjarte Johansen <bjarte.johansen@gmail.com>
2016-04-07 15:58:05 +0100

    Only visit the c code when we have to
    Remove pythons access to the c objects, we don't need it.

--- cg3/core.pyx
+++ cg3/core.pyx
@@ -12,6 +12,23 @@ from pathlib import Path
 from re import fullmatch, findall
 from tempfile import mkstemp
 
+class Document:
+
+    def __init__(self, doc):
+        self.doc = doc
+
+    def __iter__(self):
+        return iter(self.doc)
+
+    def __str__(self):
+        output = []
+        for cohort, readings in self.doc:
+            output.append(cohort)
+            for lexeme, part_of_speech in readings:
+                output.append('\t{} {}'.format(lexeme, ' '.join(part_of_speech)))
+        return '\n'.join(output)
+
+
 # We are using a context manager instead of a decorator because cython
 # functions and methods can't have python decorators without having
 # trouble with the return type.
@@ -49,210 +66,6 @@ def cg3_error():
         # Remove the file from the OS.
         os.remove(path)
 
-
-# Is this class really needed?
-cdef class Tag:
-    cdef c.cg3_tag* _raw
-
-    @staticmethod
-    cdef create(c.cg3_tag* raw):
-        tag = Tag()
-        tag._raw = raw
-        return tag
-
-    def __str__(self):
-        cdef bytes string = c.cg3_tag_gettext_u8(self._raw)
-        return string.decode()
-
-    def __repr__(self):
-        return '<{} "{}">'.format(self.__class__.__name__, str(self))
-
-
-# The second tag (the first is the cohort name) of a reading is the
-# lexeme, the rest are part-of-speech. Should we encode those
-# semantics?
-cdef class Reading:
-    cdef c.cg3_reading* _raw
-
-    @staticmethod
-    cdef create(c.cg3_reading* raw):
-        reading = Reading()
-        reading._raw = raw
-        return reading
-
-    def __str__(self):
-        # The first tag is the cohort name.
-        return ' '.join([str(tag) for tag in self[1:]])
-
-    def __repr__(self):
-        return '<{} {}>'.format(self.__class__.__name__, self[1])
-
-    def __len__(self):
-        cdef size_t n = c.cg3_reading_numtags(self._raw)
-        return n
-
-    # Make it possible to treat Reading as a list.
-    def __getitem__(self, key):
-        cdef c.cg3_reading* tag
-        if isinstance(key, slice):
-            l = []
-            for i in range(*key.indices(len(self))):
-                l.append(self[i])
-            return l
-
-        if isinstance(key, int):
-            if len(self) <= key:
-                raise IndexError('Reading index out of bounds')
-
-            tag = c.cg3_reading_gettag(self._raw, key % len(self))
-            return Tag.create(tag)
-
-        raise TypeError(
-            'Cohort indices must be a slice or an integer, not {}'.format(type(key))
-        )
-
-    def __iter__(self):
-        for i in range(len(self)):
-            yield self[i]
-
-    def add_tag(self, Tag tag):
-        return c.cg3_reading_addtag(self._raw, tag._raw)
-
-    def __dealloc__(self):
-        # Freed by the the cohort it belongs to.
-        pass
-
-
-cdef class Cohort:
-    cdef c.cg3_cohort* _raw
-
-    @staticmethod
-    cdef create(c.cg3_cohort* raw, Tag wordform):
-        cohort = Cohort()
-        cohort._raw = raw
-        c.cg3_cohort_setwordform(raw, wordform._raw)
-        return cohort
-
-    def __len__(self):
-        cdef size_t n
-        n = c.cg3_cohort_numreadings(self._raw)
-        return n
-
-    # Make it possible to treat cohort as a list.
-    def __getitem__(self, key):
-        cdef c.cg3_reading* reading
-        if isinstance(key, slice):
-            l = []
-            for i in range(*key.indices(len(self))):
-                l.append(self[i])
-            return l
-
-        if isinstance(key, int):
-            if len(self) <= key:
-                raise IndexError('Cohort index out of bounds')
-
-            reading = c.cg3_cohort_getreading(self._raw, key % len(self))
-            return Reading.create(reading)
-
-        raise TypeError(
-            'Cohort indices must be integers, not {}'.format(type(key))
-        )
-
-    def __str__(self):
-        wordform = self.get_wordform()
-        readings = ['\t' + str(reading) for reading in self]
-        return str(wordform) + '\n' + '\n'.join(readings)
-
-    def __iter__(self):
-        for i in range(len(self)):
-            yield self[i]
-
-    def __repr__(self):
-        return "<{} {}>".format(
-            self.__class__.__name__, str(self.get_wordform()))
-
-    def get_wordform(self):
-        cdef c.cg3_tag* tag
-        tag = c.cg3_cohort_getwordform(self._raw)
-        return Tag.create(tag)
-
-    def add_reading(self, Reading reading):
-        c.cg3_cohort_addreading(self._raw, reading._raw)
-
-    # When creating a reading it is not automatically added to the
-    # cohort. Should we change that here in python?
-    def create_reading(self):
-        cdef c.cg3_reading* reading
-        # Sets the wordform of the cohort as the first tag.
-        reading = c.cg3_reading_create(self._raw)
-        return Reading.create(reading)
-
-    def __dealloc__(self):
-        # Freed by the the document it belongs to.
-        pass
-
-
-cdef class Document:
-    cdef c.cg3_sentence* _raw
-    # Need to hold onto a reference so the document isn't garbage
-    # collected when the applicator goes out of scope.
-    cdef Applicator _app
-
-    @staticmethod
-    cdef create(c.cg3_sentence* raw, Applicator app):
-        document = Document()
-        document._raw = raw
-        document._app = app
-        return document
-
-    def __str__(self):
-        # The first cohort is <<<, we don't need that.
-        return '\n'.join([str(cohort) for cohort in self[1:]])
-
-    def __len__(self):
-        cdef size_t n
-        n = c.cg3_sentence_numcohorts(self._raw)
-        return n
-
-    # Treat the document as a list of cohorts.
-    def __getitem__(self, key):
-        cdef Cohort cohort
-
-        if isinstance(key, slice):
-            l = []
-            for i in range(*key.indices(len(self))):
-                l.append(self[i])
-            return l
-
-        if isinstance(key, int):
-            if len(self) <= key:
-                raise IndexError('Document index out of bounds')
-
-            cohort = Cohort()
-            cohort._raw = c.cg3_sentence_getcohort(self._raw, key % len(self))
-            return cohort
-
-        raise TypeError(
-            'Document indices must be integers, not {}'.format(type(key))
-        )
-
-    def __iter__(self):
-        for i in range(len(self)):
-            yield self[i]
-
-    def add_cohort(self, Cohort cohort):
-        c.cg3_sentence_addcohort(self._raw, cohort._raw)
-
-    # Created cohorts do not belong to the document and need to be
-    # freed manually. Cohorts added to a document is freed by the
-    # document.
-    def create_cohort(self, Tag wordform):
-        cdef c.cg3_cohort* cohort
-        cohort = c.cg3_cohort_create(self._raw)
-        return Cohort.create(cohort, wordform)
-
-    def __dealloc__(self):
-        c.cg3_sentence_free(self._raw)
 
 cdef class Grammar:
     cdef c.cg3_grammar* _raw
@@ -304,7 +117,7 @@ cdef class Applicator:
             tag = c.cg3_tag_create_u8(self._raw, text.encode())
         except TypeError:
             tag = c.cg3_tag_create_u8(self._raw, text)
-        return Tag.create(tag)
+        return tag
 
     def parse(self, f):
         def tokenize(string):
@@ -330,30 +143,67 @@ cdef class Applicator:
         cohort = toktype('Cohort') + many(reading)
         parser = many(cohort)
 
-        document = parser.parse(tokens)
+        doc = parser.parse(tokens)
+        return Document(doc)
 
-        cdef c.cg3_sentence* sentence = c.cg3_sentence_new(self._raw)
-        doc = Document.create(sentence, self)
+    def run_rules(self, doc):
+        cdef c.cg3_sentence* window
+        cdef c.cg3_tag* tag
+        cdef c.cg3_cohort* cohort
+        cdef c.cg3_reading* reading
 
-        for wordform, readings in document:
-            wordform = self.create_tag(wordform)
-            cohort = doc.create_cohort(wordform)
+        try:
+            window = c.cg3_sentence_new(self._raw)
+            for wordform, readings in doc:
+                tag = self.create_tag(wordform)
+                cohort = c.cg3_cohort_create(window)
+                c.cg3_cohort_setwordform(cohort, tag)
 
-            for lexeme, part_of_speech in readings:
-                lexeme = self.create_tag(lexeme)
-                reading = cohort.create_reading()
-                reading.add_tag(lexeme)
-                for pos in part_of_speech:
-                    pos = self.create_tag(pos)
-                    reading.add_tag(pos)
-                cohort.add_reading(reading)
-            doc.add_cohort(cohort)
+                for lexeme, part_of_speech in readings:
+                    tag = self.create_tag(lexeme)
+                    reading = c.cg3_reading_create(cohort)
+                    c.cg3_reading_addtag(reading, tag)
 
-        return doc
+                    for pos in part_of_speech:
+                        tag = self.create_tag(pos)
+                        c.cg3_reading_addtag(reading, tag)
 
-    def run_rules(self, Document doc):
-        with cg3_error():
-            c.cg3_sentence_runrules(self._raw, doc._raw)
+                    c.cg3_cohort_addreading(cohort, reading)
+                c.cg3_sentence_addcohort(window, cohort)
+
+            with cg3_error():
+                c.cg3_sentence_runrules(self._raw, window)
+
+            doc = []
+            n = c.cg3_sentence_numcohorts(window)
+            # The first cohort is >>>, we don't need that.
+            for i in range(1, n):
+                cohort = c.cg3_sentence_getcohort(window, i)
+                n = c.cg3_cohort_numreadings(cohort)
+                readings = []
+                for j in range(n):
+                    reading = c.cg3_cohort_getreading(cohort, j)
+                    n = c.cg3_reading_numtags(reading)
+
+                    part_of_speech = []
+                    # The first one is the cohort name, the second the
+                    # lexeme.
+                    for k in range(2, n):
+                        tag = c.cg3_reading_gettag(reading, k)
+                        name = c.cg3_tag_gettext_u8(tag).decode()
+                        part_of_speech.append(name)
+
+                    tag = c.cg3_reading_gettag(reading, 1)
+                    name = c.cg3_tag_gettext_u8(tag).decode()
+                    readings.append((name, part_of_speech))
+
+                tag = c.cg3_cohort_getwordform(cohort)
+                name = c.cg3_tag_gettext_u8(tag).decode()
+                doc.append((name, readings))
+
+            return Document(doc)
+        finally:
+            c.cg3_sentence_free(window)
 
     def __dealloc__(self):
         c.cg3_applicator_free(self._raw)
--- test/parse.py
+++ test/parse.py
@@ -1,9 +1,8 @@
 from cg3 import Applicator
 
-
 if __name__ == '__main__':
     applicator = Applicator('test/bm_morf-prestat-unicode.cg')
     with open('test/test.cg') as f:
         document = applicator.parse(f)
-    applicator.run_rules(document)
-    print(str(document))
+    document = applicator.run_rules(document)
+    print(document)
